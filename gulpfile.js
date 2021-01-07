2631f0976ad057dd12c3552f078f2a05b4b9f21c
aziza <aaitzidane@gmail.com>
2021-01-07 14:19:00 +0100

    lesso one to 0

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require("gulp"),
+    pug = require("gulp-pug"),
+    concat = require("gulp-concat"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer");
+    gulp.task("html", function () {
+        return gulp.src("project/html/*.pug")
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest("dist"))
+            
+        
+    });
+    gulp.task("css", function () {
+        return gulp.src(["project/css/**/*.css","project/css/**/*.scss"])
+            .pipe(sass({
+                outputStyle: "compressed"
+            }))
+            .pipe(autoprefixer())
+           .pipe(concat("main.css"))
+            .pipe(gulp.dest("dist/css"))
+            
+        
+    });
+
+
+gulp.task("watch", function () {
+    // require("./server.js")
+    gulp.watch("project/html/*.pug", gulp.series("html"));
+    gulp.watch(["project/css/**/*.css","project/css/**/*.scss"] , gulp.series("css"));
+        
+});
